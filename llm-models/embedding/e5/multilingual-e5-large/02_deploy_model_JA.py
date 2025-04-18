9a8c66aa8dadad7bcc194a36362a0aa53066dff8
hiouchiy@outlook.jp <hiouchiy@outlook.jp>
2025-04-18 01:43:30 +0000

    Updated

--- llm-models/embedding/e5/multilingual-e5-large/02_deploy_model_JA.py
+++ llm-models/embedding/e5/multilingual-e5-large/02_deploy_model_JA.py
@@ -1,8 +1,8 @@
 # Databricks notebook source
 # MAGIC %md
-# MAGIC # Databricks上のMLFlowで`multilingual-e5-large`モデルを管理する
+# MAGIC # Databricksで`multilingual-e5-large`モデルをサービングする
 # MAGIC
-# MAGIC この例では、[multilingual-e5-large model](https://huggingface.co/intfloat/multilingual-e5-large)を `sentence_transformers` フレーバーで MLFLow にロギングし、Unity Catalog でモデルを管理し、モデル提供エンドポイントを作成する方法を示します。
+# MAGIC このノートブックでは、[multilingual-e5-large model](https://huggingface.co/intfloat/multilingual-e5-large)を `sentence_transformers` フレーバーで MLFLow にロギングし、Unity Catalog でモデルを管理し、サービングエンドポイントを作成する方法を示します。
 # MAGIC
 # MAGIC このノートブックの環境
 # MAGIC - ランタイム: サーバーレスノートブック
@@ -10,8 +10,25 @@
 
 # COMMAND ----------
 
-# MAGIC %pip install sentence_transformers==4.1.0 mlflow==2.21.3
+# MAGIC %md
+# MAGIC ## ライブラリのインストール
+
+# COMMAND ----------
+
+# MAGIC %pip install mlflow==2.21.3 torch==2.5.1 sentence_transformers==4.1.0
 # MAGIC dbutils.library.restartPython()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## 変数をセット
+
+# COMMAND ----------
+
+CATALOG = "hiroshi"
+SCHEMA = "models"
+MODEL_NAME = "multilingual-e5-large"
+ENDPOINT_NAME = MODEL_NAME + "-embedding"
 
 # COMMAND ----------
 
@@ -20,10 +37,20 @@
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC まずはモデルをダウンロード
+
+# COMMAND ----------
+
 from sentence_transformers import SentenceTransformer
 model_name = "intfloat/multilingual-e5-large"
 
 model = SentenceTransformer(model_name)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 続いて、モデルのシグネイチャー（入力と出力の形式）を定義
 
 # COMMAND ----------
 
@@ -41,27 +68,19 @@ signature
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC MLFlowの`sentence_transformers` フレーバーで MLFlow にロギング
+
+# COMMAND ----------
+
 # MLFlowのSentence Transformerフレーバーを使って登録
 with mlflow.start_run() as run:  
     mlflow.sentence_transformers.log_model(
       model, 
-      "multilingual-e5-large-embedding", 
+      artifact_path=MODEL_NAME, 
       signature=signature,
       input_example=sentences,
-      extra_pip_requirements=["torch==2.6.0", "torchvision==0.21.0"]) # Obviously set specific version to avoid a deployment error
-
-# COMMAND ----------
-
-run_id = run.info.run_id
-model_uri = f"runs:/{run_id}/model"
-
-mlflow.models.predict(
-  model_uri=model_uri,
-  input_data=sentences,
-  # content_type="json",
-  # env_manager="uv",
-  # pip_requirements_override=["pillow==10.3.0", "scipy==1.13.0"],
-)
+      extra_pip_requirements=["torchvision==0.20.1"]) # Obviously install Torchvision with specific version corresponding to Torch's version to avoid a deployment error
 
 # COMMAND ----------
 
@@ -80,17 +99,21 @@ mlflow.set_registry_uri("databricks-uc")
 
 # COMMAND ----------
 
-# MAGIC %sql
-# MAGIC USE CATALOG hiroshi;
-# MAGIC CREATE SCHEMA IF NOT EXISTS models;
+# SQLクエリをPython文字列として構築
+use_catalog_query = f"USE CATALOG {CATALOG};"
+create_schema_query = f"CREATE SCHEMA IF NOT EXISTS {SCHEMA};"
+
+# SQLクエリを実行
+spark.sql(use_catalog_query)
+spark.sql(create_schema_query)
 
 # COMMAND ----------
 
 # Unityカタログへのモデル登録
 
-registered_name = "hiroshi.models.multilingual-e5-large" # UCモデル名は<カタログ名>.<スキーマ名>.<モデル名>のパターンに従っており、カタログ名、スキーマ名、登録モデル名に対応していることに注意してください。
+registered_name = f"{CATALOG}.{SCHEMA}.{MODEL_NAME}" # UCモデル名は<カタログ名>.<スキーマ名>.<モデル名>のパターンに従っており、カタログ名、スキーマ名、登録モデル名に対応していることに注意してください。
 result = mlflow.register_model(
-    "runs:/"+run.info.run_id+"/multilingual-e5-large-embedding",
+    "runs:/"+run.info.run_id+"/"+MODEL_NAME,
     registered_name,
 )
 
@@ -129,13 +152,13 @@ loaded_model.predict(
 
 # COMMAND ----------
 
-# サービングエンドポイントの名前を指定
-endpoint_name = 'multilingual-e5-large-embedding'
+databricks_url = dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiUrl().getOrElse(None)
+token = dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiToken().getOrElse(None)
 
 # COMMAND ----------
 
-databricks_url = dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiUrl().getOrElse(None)
-token = dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiToken().getOrElse(None)
+# MAGIC %md
+# MAGIC デプロイ開始
 
 # COMMAND ----------
 
@@ -143,7 +166,7 @@ from mlflow.deployments import get_deploy_client
 
 model_version = result  # mlflow.register_modelの返された結果
 
-serving_endpoint_name = endpoint_name
+serving_endpoint_name = ENDPOINT_NAME
 latest_model_version = model_version.version
 model_name = model_version.name
 
@@ -165,12 +188,17 @@ endpoint = client.create_endpoint(
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC デプロイが完了するのをMax 120分待機。多くの場合、20-30分ほどで完了します。
+
+# COMMAND ----------
+
 from datetime import timedelta
 from databricks.sdk import WorkspaceClient
 from databricks.sdk.service.serving import EndpointCoreConfigInput, ServedModelInput, ServedModelInputWorkloadSize, ServedModelInputWorkloadType
 
 w = WorkspaceClient()
-w.serving_endpoints.wait_get_serving_endpoint_not_updating(name=serving_endpoint_name, timeout=timedelta(minutes=60))
+w.serving_endpoints.wait_get_serving_endpoint_not_updating(name=serving_endpoint_name, timeout=timedelta(minutes=120))
 
 # COMMAND ----------
 
@@ -184,11 +212,12 @@ import mlflow.deployments
 client = mlflow.deployments.get_deploy_client("databricks")
 
 embeddings_response = client.predict(
-    endpoint=endpoint_name,
+    endpoint=ENDPOINT_NAME,
     inputs={
         "inputs": ["おはようございます"]
     }
 )
+print(i)
 embeddings_response['predictions']
 
 # COMMAND ----------
@@ -198,7 +227,7 @@ import time
 start = time.time()
 
 # If you get timeout error (from the endpoint not yet being ready), then rerun this.
-endpoint_response = w.serving_endpoints.query(name=endpoint_name, dataframe_records=['こんにちは', 'おはようございます'])
+endpoint_response = w.serving_endpoints.query(name=ENDPOINT_NAME, dataframe_records=['こんにちは', 'おはようございます'])
 
 end = time.time()
 
